f16900ab8582597700fe564d38f5145d06470ff3
ACherniske <apcherniske@gmail.com>
2026-04-16 22:02:09 -0400

    feat: initialize project structure with Gradle and basic application setup

---
+++ FiveNightsAtDana/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module FiveNightsAtDana.main {
+    requires java.base;
+    requires java.desktop;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports org.five_nights_at_dana;
+}
