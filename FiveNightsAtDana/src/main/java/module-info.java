1cc16696c6d86fa34ae23e9c013d3374156e8a11
ACherniske <apcherniske@gmail.com>
2026-04-17 03:28:02 -0400

    build: Fix src files for gradle build script

--- FiveNightsAtDana/src/main/java/module-info.java
+++ FiveNightsAtDana/src/main/java/module-info.java
@@ -1,9 +1,7 @@
-module FiveNightsAtDana.main {
+module FiveNightsAtDana {
     requires java.base;
     requires java.desktop;
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-
-    exports org.five_nights_at_dana;
 }
