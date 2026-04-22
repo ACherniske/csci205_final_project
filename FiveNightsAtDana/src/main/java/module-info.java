981588756b4804ac8368e7846fc2bac61e4d57a1
Fares <fol001@bucknell.edu>
2026-04-21 23:54:12 -0400

    did stuff cause turns out my / or projects file structuring was off or something, also fixed some student manager stuff

--- FiveNightsAtDana/src/main/java/module-info.java
+++ FiveNightsAtDana/src/main/java/module-info.java
@@ -3,7 +3,4 @@ module csci205_final_project  {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-
-    exports org.five_nights_at_dana;
-    exports org.five_nights_at_dana.AI;
 }
